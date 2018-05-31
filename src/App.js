09173100cbeb68a5e13b329b352f9372838152c9
Fabian Pasin <fabian.pasin@gmail.com>
2018-05-31 14:07:55 +0200

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+import { css } from 'emotion'
+import BudgetHead from 'BudgetHead'
+import Spendings from 'Spendings'
+import moduleName from 'LunchButton'
+
+class App extends Component {
+  render() {
+    return <div>App</div>
+  }
+}
+
+export default App
